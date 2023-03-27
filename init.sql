43cec3f51fe79111bf98347a7213d458dee51df1
rajikaimal <inbox.rajika@gmail.com>
2023-03-28 00:40:41 +0800

    remove create db

--- init.sql
+++ init.sql
@@ -7,8 +7,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE tchadmin;
-
 CREATE TABLE `registers` (
   `teacher_id` bigint unsigned NOT NULL,
   `teacher_email` varchar(191) NOT NULL,
