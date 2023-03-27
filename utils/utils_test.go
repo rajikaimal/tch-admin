41a756130fe7a1a62815ad997307fa05d84c88f9
rajikaimal <inbox.rajika@gmail.com>
2023-03-27 16:39:00 +0800

    test for email func

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -15,3 +15,36 @@ func TestContains(t *testing.T) {
 		t.Errorf("Expected slice not to contain value '%s', it did", value)
 	}
 }
+
+func TestIsValidEmail(t *testing.T) {
+	testCases := []struct {
+		email   string
+		wantRes bool
+	}{
+		{
+			email:   "john.snow@example.com",
+			wantRes: true,
+		},
+		{
+			email:   "jane.snow@example.co.uk",
+			wantRes: true,
+		},
+		{
+			email:   "jane.snow!!!!*(*(&@example.co.uk",
+			wantRes: false,
+		},
+		{
+			email:   "invalid_email",
+			wantRes: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.email, func(t *testing.T) {
+			gotRes := IsValidEmail(tc.email)
+			if gotRes != tc.wantRes {
+				t.Errorf("IsValidEmail(%v) = %v, want %v", tc.email, gotRes, tc.wantRes)
+			}
+		})
+	}
+}
